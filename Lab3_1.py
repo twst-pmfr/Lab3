a306a100fa43961ac3398f0201fe90d27887eb29
twst-pmfr <Nastya.stash@mail.ru>
2025-04-18 20:46:54 +0300

    Add files via upload

--- Lab3_1.py
+++ Lab3_1.py
@@ -1,7 +1,17 @@
 class Student:
-    def __init__(self, name, student_id):
-        self.name = name
-        self.student_id = student_id
-        self.grades = []
+    def __init__(self, name:str, student_id: int, grades:list):
+        self.__name = name
+        self.__student_id = student_id
+        self.__grades = grades
+    def add_grade(self, grade: int):
+        if 0 <= grade <= 10:
+            self.__grades.append(grade)
+    def get_average(self):
+        if self.__grades != []:
+            return f'Средний балл:{sum(self.__grades)/len(self.__grades)}'
+    def display_info(self):
+        return f'Имя: {self.__name};\nНомер студента: {self.__student_id};\nОценка: {self.__grades}'
 
-    def add_grade(self, grade):
+Nastya = Student(name='Настя', student_id=5, grades=[7,10,4])
+print(Nastya.get_average())
+print(Nastya.display_info())
